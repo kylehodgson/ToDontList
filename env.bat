19886e8f030ccf21afbbe5342618d091b2848665
Kyle Hodgson <kyle.hodgson@gmail.com>
2013-05-19 00:46:05 -0400

    making environment file more modular

--- env.bat
+++ env.bat
@@ -1,5 +1,6 @@
-set PATH=%PATH%;c:\Users\khodgson\Projects\knockout-presentation\bin
-set CHROME_BIN=C:\Users\khodgson\AppData\Local\Google\Chrome\Application\chrome.exe
+set HOME=c:\users\khodgson\
+set PATH=%PATH%;%HOME%\Projects\knockout-presentation\bin
+set CHROME_BIN=%HOME%\AppData\Local\Google\Chrome\Application\chrome.exe
 set IE_BIN=C:\Program Files\Internet Explorer\iexplore.exe
-set PHANTOMJS_BIN=C:\Users\khodgson\AppData\Roaming\npm\phantomjs.cmd
+set PHANTOMJS_BIN=%HOME%\AppData\Roaming\npm\phantomjs.cmd
 set FIREFOX_BIN=C:\Program Files (x86)\Mozilla Firefox\firefox.exe
